76a45fa9430462d1add60d40063f2b66f3c29a3f
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2022-07-13 20:47:18 +0530

    Add files via upload

---
+++ 03_Basics/01_ArithmeticOperations/e-temparature-conversion.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+/*
+Fahrenheit into Celsius
+        C = (F - 32) * 5/9
+*/
+func main() {
+	const F float32 = 100
+	const C float32 = (F - 32) * 5 / 9
+
+	fmt.Printf(`
+		Fahrenheit = %.2f
+		Celsius    = %.3f
+	`, F, C)
+
+	var F1 float32
+	fmt.Print("Please Enter temperature in degrees Fahrenheit:")
+	fmt.Scanf("%f", &F1)
+
+	C1 := (F1 - 32) * 5 / 9
+	fmt.Printf(`
+		Fahrenheit = %.2f
+		Celsius    = %.3f
+	`, F1, C1)
+}
+
+/*
+asignments:
+	1) WAP to implement celcius to fahrenheit conversion
+	2) WAP to take two values in runtime and compute +, -, *, /
+
+*/
